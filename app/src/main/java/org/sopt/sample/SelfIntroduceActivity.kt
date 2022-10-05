b85c122aac3c066521b25c7cf2d1b279db1068d6
hajeong <happykhj67@naver.com>
2022-10-06 00:55:14 +0900

    seminar1

--- app/src/main/java/org/sopt/sample/SelfIntroduceActivity.kt
+++ app/src/main/java/org/sopt/sample/SelfIntroduceActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import org.sopt.sample.databinding.ActivitySelfIntroduceBinding
@@ -12,13 +13,16 @@ import org.sopt.sample.databinding.ActivitySignUpBinding
 
 class SelfIntroduceActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySelfIntroduceBinding
-    private lateinit var getResultText: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySelfIntroduceBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //intent.putExtra("id")
+        val id = intent.getStringExtra("id")
+        val mbti = intent.getStringExtra("mbti")
+
+        binding.tvGetName.text= "$id"
+        binding.tvGetMbti.text= "$mbti"
     }
 }
