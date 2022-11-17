c6e839db845bf56feda0a418aed1c3d498a6c744
hajeong <happykhj67@naver.com>
2022-11-18 02:19:09 +0900

    signup view 구현

--- app/src/main/java/com/sopt/hajeong/auth/SignUpActivity.kt
+++ app/src/main/java/com/sopt/hajeong/auth/SignUpActivity.kt
@@ -4,30 +4,80 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.snackbar.Snackbar
+import com.sopt.hajeong.data.*
 import org.sopt.sample.databinding.ActivitySignUpBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+    private var email: String? = null
+    private var pw: String? = null
+    private var name: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnSignup.setOnClickListener {
-            if (binding.etId.text.length >= 6 && binding.etId.text.length <= 10 && binding.etPw.text.length >= 8 && binding.etPw.text.length <= 12 && binding.etMbti.text.length == 4) {
-                Snackbar.make(binding.root, "회원가입이 완료되었습니다.", Snackbar.LENGTH_SHORT).show()
-                val intent = Intent(this, SignInActivity::class.java)
-                intent.putExtra("id",binding.etId.text.toString())
-                intent.putExtra("pw",binding.etPw.text.toString())
-                intent.putExtra("mbti",binding.etMbti.text.toString())
-                setResult(Activity.RESULT_OK,intent)
-
-                finish()
-            } else {
-                Snackbar.make(binding.root, "아이디/비밀번호를 확인해주세요.", Snackbar.LENGTH_SHORT).show()
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    email = it.data?.getStringExtra("email") ?: ""
+                    pw = it.data?.getStringExtra("pw") ?: ""
+                    name = it.data?.getStringExtra("name") ?: ""
+                }
             }
+
+        signupBtnOnClick()
+    }
+
+    private fun signupBtnOnClick() {
+        binding.btnSignup.setOnClickListener {
+            val requestSignupDTO = RequestSignupDTO(
+                binding.etEmail.text.toString(),
+                binding.etPw.text.toString(),
+                binding.etName.text.toString()
+            )
+
+            // 회원가입 API 연결
+            val signupService : Call<ResponseSignupDTO> =ApiFactory.authService.signup(requestSignupDTO)
+
+            signupService.enqueue(object : Callback<ResponseSignupDTO> {
+                override fun onResponse(
+                    call: Call<ResponseSignupDTO>,
+                    response: Response<ResponseSignupDTO>
+                ) {
+                    //회원가입 성공
+                    if (response.isSuccessful) {
+                        Toast.makeText(this@SignUpActivity, "회원가입 성공", Toast.LENGTH_SHORT)
+                            .show()
+
+                        val intent = Intent(this@SignUpActivity, SignInActivity::class.java).apply{
+                            putExtra("id", response.body()?.result?.id)
+                        }
+                        startActivity(intent) //화면전환
+                        finish()
+                    }
+                    //회원가입 실패
+                    else {
+                        Toast.makeText(this@SignUpActivity, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseSignupDTO>, t: Throwable) {
+                    //서버통신 오류
+                    Log.e("asdf", "${t}")
+                    Toast.makeText(this@SignUpActivity, "에러 발생", Toast.LENGTH_SHORT).show()
+                }
+            })
         }
     }
 }
